590227a2cf15b16297858b28bdd9b39bbf56f525
yukimura-manase <masahiro19951212@gmail.com>
2021-08-19 09:40:18 +0900

    DBにデータ追加

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -5,16 +5,11 @@
         <div><img :src="itemdetails.imageURL"> </div>
         <div>{{itemdetails.name}}</div>
         <div>{{itemdetails.contents}}</div>
-        <div>【トッピング】</div>
-            <span v-for="tops in toppings" :key="tops.ID">
-            <label><input type="checkbox" v-model="choseToppings" :value="tops.name">
-            {{ tops.name }}</label></span> 
-            <div>※Mサイズは1トッピングにつき200円</div>
-            <div>Lサイズは1トッピングにつき300円</div>
+        
         <div>【サイズ】</div>
         <div>M: {{ itemdetails.priceM }}円(税抜)</div>
         数量：<select v-model="countM">
-                <option disabled>0</option>
+                <option>0</option>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
@@ -28,7 +23,7 @@
             </select>
         <div>L:{{ itemdetails.priceL }}円(税抜)</div>
         数量：<select v-model="countL">
-                <option disabled>0</option>
+                <option>0</option>
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
@@ -40,6 +35,13 @@
                 <option>9</option>
                 <option>10</option>
             </select>
+
+            <div>【トッピング】</div>
+            <span v-for="tops in toppings" :key="tops.ID">
+            <label><input type="checkbox" v-model="choseToppings" :value="tops.name">
+            {{ tops.name }}</label></span> 
+            <div>※Mサイズは1トッピングにつき200円</div>
+            <div>Lサイズは1トッピングにつき300円</div>
         <div>選択中のトッピング：{{ choseToppings }}</div>
         <h1>商品金額：{{  totalPrice }}円(税込)</h1>
         <router-link :to="{name: 'Home'}"><button>戻る</button></router-link> |
@@ -97,14 +99,46 @@ export default {
     },
 
     computed:{
+
+
+        // トッピングの料金を追加 
+
+        toppingTotal(){
+            return (this.choseToppings.length * 200) * this.countM + (this.choseToppings.length * 300) * this.countL
+        },
+        
+
+        
+
         totalPrice(){
-            const total = (this.itemdetails.priceM * this.countM + this.itemdetails.priceL * this.countL) * 1.1
+            const total = (this.itemdetails.priceM * this.countM + this.itemdetails.priceL * this.countL +this.toppingTotal ) * 1.1
             return Math.floor(total)
         },
         ...mapState(['toppings'])
     }
 }
 
+// 【ユースケース】
+// 商品詳細を表示する
+// 【アクタ】
+// 利用者
+// 【概要】
+// 選択された商品の詳細情報を表示する
+// 利用者が選択した内容から金額を表示
+// 【前提条件】
+// 商品情報が存在すること
+// 商品一覧画面が表示されていること
+// 【正常フロー】
+// (1)利用者は商品名の画像や、詳細表示のボタンを押下する
+// (2)システムは選択された商品の詳細情報を表示する
+// (3)利用者は数量を選択する
+// (4)システムは画面下部に金額を
+//  リアルタイムで表示する
+// 【代替フロー】
+//  特になし
+// 【事後条件】
+//  商品情報詳細ページに、
+//  商品詳細情報と合計金額が表示される
 
 
 
